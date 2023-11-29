fb576571fca8f297b79cb7a5aac83b442c489a75
JervNorsk <jerv.norsk@gmail.com>
2023-11-29 13:05:25 +0100

    Implemented Google Play Services integration with Unity plugin v10.14

--- Assets/Scripts/GPGSAuthentication.cs
+++ Assets/Scripts/GPGSAuthentication.cs
@@ -7,41 +7,38 @@ public class GPGSAuthentication : MonoBehaviour
 {
     void Start()
     {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+            // enables saving game progress.
+            .EnableSavedGames()
+            // requests the email address of the player be available.
+            // Will bring up a prompt for consent.
+            .RequestEmail()
+            // requests a server auth code be generated so it can be passed to an
+            //  associated back end server application and exchanged for an OAuth token.
+            .RequestServerAuthCode(false)
+            // requests an ID token be generated.  This OAuth token can be used to
+            //  identify the player to other services such as Firebase.
+            .RequestIdToken()
+            .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
         PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
         PlayGamesPlatform.Activate();
+
+        // Authenticate user:
+        PlayGamesPlatform.Instance.Authenticate(SignInInteractivity.CanPromptOnce, (result) =>{
+            // handle results
+            if (result == SignInStatus.Success)
+            {
+                Debug.Log("Sign In successed!");
+            }
+            else
+            {
+                Debug.LogError("Sign In failed!");
+            }
+        });
     }
 
-    void Native()
-    {
-        PlayGamesPlatform.Instance.Authenticate(NativeProcessAuthentication);
-    }
-
-    void Integration()
-    {
-        Social.localUser.Authenticate(IntegrationProcessAuthentication);
-    }
-
-    internal void NativeProcessAuthentication(SignInStatus status)
-    {
-        if (status == SignInStatus.Success)
-        {
-            IntegrationProcessAuthentication(true);
-        }
-        else
-        {
-            IntegrationProcessAuthentication(false);
-        }
-    }
-
-    internal void IntegrationProcessAuthentication(bool status)
-    {
-        if (status == true)
-        {
-            Debug.Log("Logged in successfuly!");
-        }
-        else
-        {
-            Debug.LogError("Logged in failed!");
-        }
-    }
 }
